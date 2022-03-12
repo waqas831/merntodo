f5bc596ac8618d9294925a6e4a3118a902629dce
mohammad waqas <57134498+waqas831@users.noreply.github.com>
2022-03-12 11:05:50 +0500

    resolve apis proxy

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const bodyParser=require('body-parser');
 const port =process.env.PORT || 8080;
 const route = require("./routes/index");
 const cors=require('cors');
+const path=require('path');
 
 app.use(bodyParser.json());
 app.use(express.json())
@@ -14,6 +15,10 @@ app.use("/", route);
 
 if(process.env.NODE_ENV=='production'){
   app.use(express.static('todo/build'));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "todo", "build", "index.html"));
+  });
 }
 
 app.listen(port, (err) => {
